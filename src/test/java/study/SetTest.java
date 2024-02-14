df0c22000a5e58500976370bcaebb1571c304c0b
CHOHYUNHWA <gusghk115@gmail.com>
2024-02-14 15:30:56 +0900

    study: JUnit5 Test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,50 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(0);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+
+    @Test
+    @DisplayName("numbers 사이즈 확인")
+    void testNumbersSize() {
+        assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    @DisplayName("numbers contains Test")
+    void contains(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("numbers contains Test2")
+    void contains2(int element, boolean expected) {
+        assertThat(numbers.contains(element)).isEqualTo(expected);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,53 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void divideOneDotTwo() {
+        String[] actualArray = "1,2".split(",");
+        assertThat(actualArray).contains("1", "2");
+        assertThat(actualArray).containsExactly("1", "2");
+    }
+
+    @Test
+    void divideOnlyOne() {
+        String[] actualArray = "1".split(",");
+        assertThat(actualArray).contains("1");
+        assertThat(actualArray).containsExactly("1");
+    }
+
+    @Test
+    void divideOneDotTwoInParentheses() {
+        String beforeString = "(1,2)";
+        String actual = beforeString.substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열의 범위를 벗어났을 때 에러 테스트")
+    void charAtTest(){
+        String testString = "abc";
+        assertThatThrownBy(() -> {
+            testString.charAt(testString.length());
+        })
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range")
+                .hasMessageContaining(String.valueOf(testString.length()));
+
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    testString.charAt(testString.length());
+                }).withMessageMatching("String index out of range: \\d");
     }
 }
